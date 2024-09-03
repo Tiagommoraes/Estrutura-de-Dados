dc076d7170c06ed52a53cb1bc7a9c9159d119c0d
Tiago Martins Moraes <142947043+Tiagommoraes@users.noreply.github.com>
2024-09-02 21:47:14 -0300

    Add files via upload

---
+++ Vetor.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Vetor{
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int[] numeros = new int[10];
+        System.out.println("Digite 10 números inteiros: ");
+
+        for (int i = 0; i < numeros.length; i++){
+            numeros[i] = scn.nextInt();
+        }
+        System.out.println("Os números pares são: ");
+        for (int numero : numeros){
+            if (numero % 2 == 0){
+                System.out.println(numero + " ");
+            }
+        }
+        System.out.println("Os números ímpares são: ");
+        for (int numero : numeros){
+            if (numero % 2 != 0){
+                System.out.println(numero + " ");
+            }
+        }
+        scn.close();
+    }
+}

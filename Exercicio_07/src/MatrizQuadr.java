0721fd7e86bcef06d9b03d1de8ae9182f2c6642b
Tiago Martins Moraes <142947043+Tiagommoraes@users.noreply.github.com>
2024-09-14 17:09:03 -0300

    Update MatrizQuadr.java

--- Exercicio_07/src/MatrizQuadr.java
+++ Exercicio_07/src/MatrizQuadr.java
@@ -1,4 +1,4 @@
-/*Escreva um programa que receba um número inteiro 2 ≤ 𝑁 ≤ 5, crie uma
+/*07) - Escreva um programa que receba um número inteiro 2 ≤ 𝑁 ≤ 5, crie uma
 matriz quadrada 𝑁 × 𝑁, preencha a matriz com valores de 1 até 𝑁2, calcule o
 quadrado da matriz criada e exiba o resultado na tela.*/
 

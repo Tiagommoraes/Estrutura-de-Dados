4e3b141ccd0f877182492fd50b62af2ef4867a82
Tiago Martins Moraes <142947043+Tiagommoraes@users.noreply.github.com>
2024-10-07 00:16:15 -0300

    Update Ordenacao.java

--- Atividade_05_Ordenacao/src/Ordenacao.java
+++ Atividade_05_Ordenacao/src/Ordenacao.java
@@ -1,5 +1,6 @@
-/*05) - Implemente dois algoritmos que ordenem os elementos de um vetor em ordem crescente e compare o número de trocas que eles efetuam
-durante a ordenação de:
+/*05) - Implemente dois algoritmos que ordenem os elementos de
+um vetor em ordem crescente e compare o número de trocas que
+eles efetuam durante a ordenação de:
 a) um vetor criado com os números de 1 até 100 aleatoriamente distribuídos.
 b) um vetor criado com os números de 1 até 100 ordenados em ordem decrescente.
 */

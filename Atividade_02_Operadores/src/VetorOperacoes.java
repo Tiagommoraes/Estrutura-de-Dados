26a75c0bf0e5d122fb04fdf1c6c5a84963a15cbd
Tiago Martins Moraes <142947043+Tiagommoraes@users.noreply.github.com>
2024-10-07 00:02:54 -0300

    Update VetorOperacoes.java

--- Atividade_02_Operadores/src/VetorOperacoes.java
+++ Atividade_02_Operadores/src/VetorOperacoes.java
@@ -1,7 +1,11 @@
-/*02) - Escreva um programa que carregue um vetor inteiro de cem posições com números aleatórios entre 0 e 100. Percorrendo o vetor criado
-apenas uma vez, imprima a posição onde ocorre o menor o valor, a soma dos números armazenados e preencha os valores de um novo vetor
-com metade do tamanho do vetor original onde a primeira posição do novo vetor é igual à soma da primeira e da última posição do vetor
-original. A segunda posição do novo vetor é a soma da segunda e da penúltima posição do vetor original e assim em diante.*/
+/*02) - Escreva um programa que carregue um vetor inteiro de cem posições 
+com números aleatórios entre 0 e 100. Percorrendo o vetor criado
+apenas uma vez, imprima a posição onde ocorre o menor o valor, 
+a soma dos números armazenados e preencha os valores de um novo vetor
+com metade do tamanho do vetor original onde a primeira posição do novo 
+vetor é igual à soma da primeira e da última posição do vetor
+original. A segunda posição do novo vetor é a soma da segunda e da penúltima
+posição do vetor original e assim em diante.*/
 
 import java.util.Random;
 public class VetorOperacoes {

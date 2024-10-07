6a892e6ed202fb2dacffba53343c1c8216bfc87b
Tiago Martins Moraes <142947043+Tiagommoraes@users.noreply.github.com>
2024-10-07 00:09:36 -0300

    Update VetParImpar.java

--- Atividade_03_ParesImpares/src/VetParImpar.java
+++ Atividade_03_ParesImpares/src/VetParImpar.java
@@ -1,7 +1,8 @@
-/*03) - Escreva um programa que carregue dois vetores inteiros com 5 posições, sendo um com números pares e o outro com números ímpares. O
-usuário pode digitar os números em qualquer sequência e o programa deverá armazená-los no vetor correto na ordem em que foram informados
-pelo usuário.*/
-
+/*03) - Escreva um programa que carregue dois vetores
+inteiros com 5 posições, sendo um com números pares e 
+o outro com números ímpares. O usuário pode digitar os 
+números em qualquer sequência e o programa deverá armazená-los
+no vetor correto na ordem em que foram informados pelo usuário.*/
 import java.util.Scanner;
 public class VetParImpar {
     public static void main(String[] args) {
